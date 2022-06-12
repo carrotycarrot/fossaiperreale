7f1fe40ab6d54bfb0e4f0efbcdf8b34a6ddae8b2
Samuele Illuminati <samuele8@gmail.com>
2022-06-12 15:29:35 +0200

    fix fossa vr

--- app.js
+++ app.js
@@ -3,23 +3,37 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
 const router = express.Router();
 
+var app = express();
+app.use(router);
 app.use(express.static(__dirname + '/public'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+router.get('/fossavr/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/fossavr/index.html'));
+});
+
+router.get('/fossavr/Build/:filename',function(req,res){
+  var options = { }
+  if(req.params.filename.endsWith(".gz")){
+    options["headers"] = {
+      'Content-Encoding': 'gzip',
+      'Content-Type': 'application/x-gzip'
+    }
+  }
+  if(req.params.filename.endsWith(".js")){
+    options["headers"] = {
+      'Content-Type': 'text/javascript'
+    }
+  }
+  res.sendFile(path.join(__dirname+'/public/fossavr/Build/'+req.params.filename), options);
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,8 +52,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-app.use('/', router);
 
-router.get('/fossavr',function(req,res){
-  res.sendFile(path.join(__dirname+'/fossavr/index.html'));
-});
